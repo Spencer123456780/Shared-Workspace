95f593e16cdfcbd8c648273ce36217486c78ca16
EffieStella <95853951+EffieStella@users.noreply.github.com>
2022-04-06 17:45:55 -0600

    Update JavaScript.js
    work in progress

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,85 @@
-Java Script Stuff
+import express from 'express';
+var app = express();
+// start the server in the port 3001 !
+var callback = function () {
+  
+}
+app.listen(4001, callback);
+
+
+
+// on the request to root (localhost:3001/)
+app.get('/', function (req, res) {
+    res.send();
+});
+
+// On localhost:3001/welcome
+app.get('/welcome', function (req, res) {
+    res.send();
+});
+
+// On localhost:3001/login
+app.get('/login', function (req, res) {
+    res.send('<p>username<input type="text" /></p>  <p>Password: <input type="text" /> </p> <button>login</button>');
+});
+
+
+
+const app = express()
+app.use(express.json())
+
+exports.register = async (req, res, next) => {
+    const { username, password } = req.body
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Enter password" })
+    }
+    try {
+      await User.create({
+        username,
+        password,
+      }).then(user =>
+        res.status(200).json({
+          message: "User successfully created",
+          user,
+        })
+      )
+    } catch (err) {
+      res.status(401).json({
+        message: "User not successful created",
+        error: error.mesage,
+      })
+    }
+  }
+
+  const express = require("express")
+const router = express.Router()
+const { register } = require("./auth")
+router.route("/register").post(register)
+module.exports = router
+
+exports.login = async (req, res, next) => {
+    const { username, password } = req.body
+    // Check if username and password is provided
+    if (!username || !password) {
+      return res.exports.login = async (req, res, next) => {
+        try {
+          const user = await User.findOne({ username, password })
+          if (!user) {
+            res.status(401).json({
+              message: "Login not successful",
+              error: "User not found",
+            })
+          } else {
+            res.status(200).json({
+              message: "Login successful",
+              user,
+            })
+          }
+        } catch (error) {
+          res.status(400).json({
+            message: "An error occurred",
+            error: error.message,
+          })
+        }
+
+      }
